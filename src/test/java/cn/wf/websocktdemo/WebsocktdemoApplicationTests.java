cf46f2a98005ac94b93a9b4d531fcb16e188d416
wang fan <fan.wang02@weimob.com>
2020-09-04 18:23:22 +0800

    websocket-demo

---
+++ src/test/java/cn/wf/websocktdemo/WebsocktdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wf.websocktdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocktdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
